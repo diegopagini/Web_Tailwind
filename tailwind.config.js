69bca22ca5aebd85b85a2db384f1e19af88fee85
Diego Pagini <diego.pagini@neoris.com>
2023-02-16 16:17:18 +0100

    logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
 			backgroundImage: (theme) => ({
 				'body-pattern': "url('../img/pattern.png')",
 			}),
+			colors: {
+				'light-blue': '#37bcf9',
+				'dark-blue': '#0370b9',
+			},
 		},
 	},
 	variants: {},
