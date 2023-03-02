9303c00ebe8f0bc51b937a7cc5b30e4cc76a77a3
Diego Pagini <diego.pagini@neoris.com>
2023-03-02 08:41:40 +0100

    animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,36 @@ module.exports = {
 				'light-blue': '#37bcf9',
 				'dark-blue': '#0370b9',
 			},
+			animation: {
+				'spin-low': 'spin 2s linear infinite',
+				'from-bellow': 'fromBellow 500ms linear',
+				'from-right': 'fromRight 300ms linear',
+			},
+			keyframes: {
+				fromBellow: {
+					'0%': {
+						transform: 'translateY(0%)',
+					},
+					'50%': {
+						transform: 'translateY(200%)',
+					},
+					'100%': {
+						transform: 'translateY(0%)',
+					},
+				},
+				fromRight: {
+					'0%': {
+						transform: 'translateX(200%)',
+					},
+					'100%': {
+						transform: 'translateX(0%)',
+					},
+				},
+			},
 		},
 	},
-	variants: {},
+	variants: {
+		animation: ['responsive', 'hover', 'group-hover'],
+		animate: ['responsive', 'hover', 'group-hover'],
+	},
 };
