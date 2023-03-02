adba4f1bf522897e7ea1ee94b7ecd1ac035b9dd8
Diego Pagini <diego.pagini@neoris.com>
2023-03-02 11:09:40 +0100

    banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,12 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundImage: (theme) => ({
-				'body-pattern': "url('../img/pattern.png')",
+				'body-pattern': "url('./img/pattern.png')",
+				banner: "url('./img/bakbaner.png')",
 			}),
+			backgroundPosition: {
+				'banner-position': '-200px -200px',
+			},
 			colors: {
 				'light-blue': '#37bcf9',
 				'dark-blue': '#0370b9',
@@ -14,6 +18,11 @@ module.exports = {
 				'spin-low': 'spin 2s linear infinite',
 				'from-bellow': 'fromBellow 500ms linear',
 				'from-right': 'fromRight 300ms linear',
+				'bg-banner': 'backBanner 10s linear',
+				'text-banner': 'showBannerText 10s linear',
+			},
+			screens: {
+				xs: { max: '768px' },
 			},
 			keyframes: {
 				fromBellow: {
@@ -33,6 +42,36 @@ module.exports = {
 					},
 					'100%': {
 						transform: 'translateX(0%)',
+					},
+				},
+				backBanner: {
+					'0%': {
+						'background-position': '0px 0px',
+					},
+					'100%': {
+						'background-position': '-200px -200px',
+					},
+				},
+				showBannerText: {
+					'0%': {
+						transform: 'translateX(-600%) scale(5,5)',
+						opacity: 1,
+					},
+					'50%': {
+						transform: 'translateX(600%) scale(5,5)',
+						opacity: 1,
+					},
+					'75%': {
+						color: 'transparent',
+						transform: 'translateX(-600%) scale(5,5)',
+						opacity: 0,
+						'text-shadow': 'none',
+					},
+					'100%': {
+						color: 'white',
+						transform: 'translateX(0%) scale(1,1)',
+						opacity: 1,
+						'text-shadow': '2px 2px 1px black',
 					},
 				},
 			},
